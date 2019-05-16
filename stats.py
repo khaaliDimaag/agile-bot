73652847e3e5f2e1af586b03f73bbee6a94d6a8a
Palash A <palash.agarwal.96@gmail.com>
2019-05-16 19:44:31 +0530

    skeleton

--- stats.py
+++ stats.py
@@ -1,13 +1,14 @@
 import os, datetime
-
 import discord
 from discord.ext import commands
+# Move inside v
 
 class khaaliStats(discord.Client):
 
-  def init(self, prefix='?'):
+  def init(self, prefix='!'):
     self.PREFIX = prefix
 
+  ''' Events '''
 
   async def on_ready(self):
     act = discord.Activity()
@@ -19,7 +20,63 @@ class khaaliStats(discord.Client):
   async def on_message(self, msg):
     pass
 
+  async def on_member_join(self, mem):
+    pass
+
+  async def on_member_remove(self, mem):
+    pass
+
+  async def on_guild_role_create(self, role):
+    pass
+
+  async def on_guild_role_delete(self, role):
+    pass  
+
+  async def on_guild_role_update(self, before, after):
+    pass
+
+  async def on_member_ban(self, guild, mem):
+    pass
+
+  async def on_member_unban(self, guild, mem):
+    pass
+
+  async def on_guild_update(self, before, after):
+    pass
+
+  async def on_webhooks_update(self, channel):
+    pass
   
+  async def on_guild_integrations_update(self, guild):
+    pass
+
+  async def on_guild_channel_delete(self, channel):
+    pass
+  
+  async def on_guild_channel_create(self, channel):
+    pass
+
+  async def on_guild_channel_update(self, before, after):
+    pass
+
+  ''' Commands '''
+
+  async def members(ctx):
+    pass
+  
+  async def channels(ctx):
+    pass
+  
+  async def roles(ctx):
+    pass
+  
+  async def all(ctx):
+    pass
+
+  async def help(ctx):
+    pass
+  
+
 
 if __name__=='__main__':
   if not os.environ.get('KHAALI_STATS'): exit('token not found')
