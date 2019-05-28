d9834ad9c915ba17bb5d030a28264f190379f8aa
Palash A <palash.agarwal.96@gmail.com>
2019-05-28 11:44:39 +0530

    someday I will know what I am doing; not today

--- cogs/stats.py
+++ cogs/stats.py
@@ -1,29 +1,72 @@
 import os, datetime
 import asyncio
+import pprint
 
 import discord
 from discord.ext import commands
-from pymongo import MongoClient # Use RDBMS?
 
-import logging
-# logging.basicConfig(format='[%(asctime)s] - (%(name)s) - %(levelname)s: %(message)s')
 
 class khaaliStats(commands.Cog):
   def __init__(self, bot, db_name='khaaliDiscord', logname='khaaliStats'):
     self.bot = bot
-    self.db = MongoClient()[db_name]
-    self.logger = logging.getLogger(logname)
-    # TODO logger setup
+    self.db = self.Database(db_name)
+    self.logger = self.Logs(logname)
+    self.talk = self.Messages()
+    self.cmds = self.Commands(bot)
 
   @commands.Cog.listener()
   async def on_ready(self):
     for guild in self.bot.guilds: 
       self.setup_guild(guild)
-    print('Lets track some spicy stats')
+    print('Mmm lets track some spicy stats')
+
+  @commands.Cog.listener()
+  async def on_resumed(self):
+    pass
 
   @commands.Cog.listener()
   async def on_message(self, msg):
     pass
+
+  @commands.Cog.listener()
+  async def on_error(self, event, *args):
+    pass
+
+
+
+  @commands.Cog.listener()
+  async def on_raw_message_delete(self, payload):
+    pass
+
+  @commands.Cog.listener()
+  async def on_raw_message_edit(self, payload):
+    pass
+
+  @commands.Cog.listener()
+  async def on_raw_bulk_message_delete(self, payload):
+    pass
+
+  @commands.Cog.listener()
+  async def on_raw_reaction_add(self, payload):
+    pass
+
+  @commands.Cog.listener()
+  async def on_raw_reaction_remove(self, payload):
+    pass
+
+  @commands.Cog.listener()
+  async def on_raw_reaction_clear(self, rx):
+    pass
+
+  @commands.Cog.listener()
+  async def on_private_channel_pins_update(self, channel, last):
+    pass
+
+  @commands.Cog.listener()
+  async def on_guild_channel_pins_update(self, channel, last):
+    pass
+
+
 
   @commands.Cog.listener()
   async def on_member_join(self, mem):
@@ -32,6 +75,16 @@ class khaaliStats(commands.Cog):
   @commands.Cog.listener()
   async def on_member_remove(self, mem):
     pass
+
+  @commands.Cog.listener()
+  async def on_member_ban(self, guild, mem):
+    pass
+
+  @commands.Cog.listener()
+  async def on_member_unban(self, guild, mem):
+    pass
+
+
 
   @commands.Cog.listener()
   async def on_guild_role_create(self, role):
@@ -45,13 +98,21 @@ class khaaliStats(commands.Cog):
   async def on_guild_role_update(self, before, after):
     pass
 
+
+
   @commands.Cog.listener()
-  async def on_member_ban(self, guild, mem):
+  async def on_guild_channel_create(self, channel):
+    pass
+    
+  @commands.Cog.listener()
+  async def on_guild_channel_delete(self, channel):
     pass
 
   @commands.Cog.listener()
-  async def on_member_unban(self, guild, mem):
+  async def on_guild_channel_update(self, before, after):
     pass
+
+
 
   @commands.Cog.listener()
   async def on_guild_update(self, before, after):
@@ -65,43 +126,101 @@ class khaaliStats(commands.Cog):
   async def on_guild_integrations_update(self, guild):
     pass
 
-  @commands.Cog.listener()
-  async def on_guild_channel_delete(self, channel):
-    pass
 
-  @commands.Cog.listener()
-  async def on_guild_channel_create(self, channel):
-    pass
-
-  @commands.Cog.listener()
-  async def on_guild_channel_update(self, before, after):
-    pass
-
-
-  @commands.command()
-  async def members(self, ctx, *args):
-    pass
-
-  @commands.command()
-  async def channels(self, ctx, *args):
-    pass
-
-  @commands.command()
-  async def roles(self, ctx, *args):
-    pass
-
-  @commands.command()
-  async def projects(self, ctx, *args):
-    pass
-
-  @commands.command()
-  async def all(self, ctx, *args):
-    pass
 
   # Update from DM instead?
   @commands.command()
   async def update(self, ctx, *args):
     pass
+
+
+  class Database():
+    ''' Schema for `khaaliDiscord`
+    [Collection] members: {
+      name: str,
+      handle: str,
+      nick: str,
+      created: date,
+      joined_first: date,
+      left: date,
+      rejoined: date,
+      roles: [],
+      bot: boolean
+    }
+    [Collection] channels: {
+      name: str,
+      id: int,
+      created: date,
+      guild_id: int,
+      private: boolean,
+      voice: boolean
+    }
+    [Collection] roles: {
+      name: str,
+      purpose: str,
+      value: int,
+      perms: [],
+      created: date
+    }
+    [Collection] guilds: {
+      name: str,
+      id: int,
+      owner_id: int,
+      stats_chan_id: int,
+      members: [],
+      channels: [],
+      roles: []
+    }'''
+    def __init__(self, db_name):
+      self.connection = None
+      try: from pymongo import MongoClient()
+      except ImportError: print('No pyMongo!')
+      else: self.connection = MongoClient()[db_name]
+
+  class Messages():
+    def __init__(self):
+      pass
+    
+    def welcome_message(self, first):
+      if first: return 'You made a wise choice! Let\'s do some stats!'
+      else: return 'Thanks for adding me back fam!'
+
+    def channel_id_embed(self):
+      em = discord.Embed()
+      # em.set_image(url='https://khaalidimaag.io/discord/khaaliStats/channel_id.gif') # NOTE: Link not active
+      em.set_footer(text=':point_up_2: Send the channel ID fam')  
+      em.set_author(name='khaaliStats',url='https://khaalidimaag.io/discord/khaaliStats')
+      return em
+
+  class Logs():
+    def __init__(self, logname):
+      import logging
+      pass
+
+  class Commands():
+    def __init__(self, bot):
+      pass
+
+    @commands.command()
+    async def members(self, ctx, *args):
+      pass
+
+    @commands.command()
+    async def channels(self, ctx, *args):
+      pass
+
+    @commands.command()
+    async def roles(self, ctx, *args):
+      pass
+
+    @commands.command()
+    async def projects(self, ctx, *args):
+      pass
+
+    @commands.command()
+    async def all(self, ctx, *args):
+      pass
+  
 
 
   def setup_guild(self, guild):
@@ -133,7 +252,7 @@ class khaaliStats(commands.Cog):
       pass
     pass
 
-  def get_stats_chan(self, guild, first=True):
+  async def get_stats_chan(self, guild, first=True):
     owner = guild.get_member(guild.owner_id)
     dm = await owner.create_dm() if not owner.dm_channel else owner.dm_channel
     def check(m): return m.channel.id == dm.id and m.author == owner_id
@@ -172,7 +291,7 @@ class khaaliStats(commands.Cog):
     b3: projects
     default: (all) = int(1111,10) = 15
   '''
-  def send_stats(channel, category=15):
+  async def send_stats(channel, category=15):
     em = discord.Embed(
       title='Current stats of {0.name}'.format(channel.guild),
       timestamp=datetime.datetime.now(),
@@ -184,3 +303,7 @@ class khaaliStats(commands.Cog):
 
 
 def setup(bot): bot.add_cog(khaaliStats(bot))
+
+if __name__=='__main__':
+  bot = commands.Bot(command_prefix='~')
+  setup(bot)
